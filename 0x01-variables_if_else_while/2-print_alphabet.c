cc95b3d1fdb89dfbf396796c977f9b5a8726a3ea
1270@holberton <1270@holbertonschool.com>
2019-09-19 23:14:46 +0000

    mandatory 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+/* headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letra;
+
+	for (letra = 'a'; letra <= 'z'; letra++)
+	{
+		putchar(letra);
+		if (letra == 'z')
+		{
+			putchar('\n'); }
+	}
+	return (0);
+}
