b86631a839ac78d8fe5a0264088a53da55df15e5
1270@holberton <1270@holbertonschool.com>
2019-09-20 15:52:17 +0000

    mandatory 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int numero;
+	int letra;
+
+	for (numero = '0'; numero <= '9'; numero++)
+	{
+		putchar(numero); }
+		for (letra = 'a'; letra <= 'f'; letra++)
+		{
+			putchar(letra); }
+		putchar('\n');
+	return (0);
+}
