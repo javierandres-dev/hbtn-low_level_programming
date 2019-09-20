fd2f886bf92be5cb14c1b25ee242cbe110868e5a
1270@holberton <1270@holbertonschool.com>
2019-09-20 01:38:00 +0000

    correction 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int numero;
+
+	for (numero = '0'; numero <= '9'; numero++)
+	{
+		putchar(numero);
+		putchar('\n');
+	}
+	return (0);
+}
