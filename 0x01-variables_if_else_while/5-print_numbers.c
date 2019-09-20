973cff41343e91cce7423811992f85dbcc18d0d3
1270@holberton <1270@holbertonschool.com>
2019-09-20 00:44:06 +0000

    mandatory 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+/* headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+ 		printf ("%d", i);
+	}
+	printf ("\n");
+	return (0);
+}
