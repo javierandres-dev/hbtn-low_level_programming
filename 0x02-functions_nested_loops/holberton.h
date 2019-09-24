5e977f862faf22fadcd2714a0797555fc8abc629
Javier Andrés <1270@holberton.com>
2019-09-24 16:33:58 +0000

    mandatory 2

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -2,7 +2,13 @@
 #define _PUTCHAR
 int _putchar(char c);
 #endif
-#ifndef _PRINT_ALPHABETH_H_
-#define _PRINT_ALPHABETH_H_
+
+#ifndef _PRINT_ALPHABET_H_
+#define _PRINT_ALPHABET_H_
 int print_alphabet(void);
+#endif
+
+#ifndef _PRINT_ALPHABET_X10_H
+#define _PRINT_ALPHABET_X10_H
+int print_alphabet_x10(void);
 #endif
