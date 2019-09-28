9949a69f63e8274890cd96686b99a4edfa5c187c
Javier Andrés <1270@holberton.com>
2019-09-28 22:33:24 +0000

    correction 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -22,7 +22,14 @@ int main(void)
 			}
 			if (n % 5 == 0)
 			{
-				printf("Buzz ");
+				if (n == 100)
+				{
+					printf("Buzz");
+				}
+				else
+				{
+					printf("Buzz ");
+				}
 			}
 		}
 		else
