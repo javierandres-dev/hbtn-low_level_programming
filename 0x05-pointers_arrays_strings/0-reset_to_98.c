2d5c09d96ba9498803307e83f17248cce65c4dc3
1270holberton <1270@holbertonschool.com>
2019-09-30 22:21:36 +0000

    task1-mandatory

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,6 +2,7 @@
 /**
  * reset_to_98 - function that takes a pointer to an int as parameter and
  * updates the value it points to to 98.
+ * @n: pointer input
  */
 void reset_to_98(int *n)
 {
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -2,3 +2,8 @@
 #define RESET_TO_98_H
 void reset_to_98(int *n);
 #endif
+
+#ifndef SWAP_INT_H
+#define SWAP_INT_H
+void swap_int(int *a, int *b);
+#endif
