cea49969b38b3d619b87f8857571b433189afe38
1270holberton <1270@holbertonschool.com>
2019-10-02 18:33:52 +0000

    task9-betty

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
-* strcpy - function that copies the string pointed to by src, including the
+* _strcpy - function that copies the string pointed to by src, including the
 * terminating null byte (\0), to the buffer pointed to by dest.
 * @dest: input
 * @src: input
