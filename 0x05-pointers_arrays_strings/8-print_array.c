e4b26203079dd8fb751a67938de761317b5de723
1270holberton <1270@holbertonschool.com>
2019-10-02 17:09:52 +0000

    task8-mandatory

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,17 +9,18 @@
  */
 void print_array(int *a, int n)
 {
-	int i = 0;
+	int i;
 
-	while (a[i] != '\0')
+	for (i = 0; i < n; i++)
 	{
-		i++;
+		if (i != n - 1)
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d", a[i]);
+		}
 	}
-	while (n > 1)
-	{
-		printf("%d, ", a[i]);
-	n--;
-	}
-	printf("%d", a[i]);
 	printf("\n");
 }
