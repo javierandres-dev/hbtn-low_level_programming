0a4ff7e27348af610456dbe3d815cf6220233fcb
1270holberton <1270@holbertonschool.com>
2019-10-02 01:40:22 +0000

    task7-correction

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,20 +8,18 @@ void puts_half(char *str)
 {
 	int i = 0;
 	int j;
-	int k;
 
 	while (str[i] != '\0')
 	{
 		i++;
 	}
-	k = i / 2;
-	i--;
-	if (k % 2 != 0)
+	if (i % 2 != 0)
 	{
-		for (j = k; j <= i; j++)
+		i++;
+	}
+	for (j = i / 2; str[j] != '\0' ;j++)
 		{
 			_putchar(str[j]);
 		}
-	}
 	_putchar('\n');
 }
