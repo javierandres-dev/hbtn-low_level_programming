98b68f075dfbc192ec4467fd0aad2020ea644025
Javier Andrés <1270@holberton.com>
2019-10-05 05:15:46 +0000

    task7-correction

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,31 +6,19 @@
  */
 char *leet(char *s)
 {
+	int l[20] = {65, 52, 97, 52, 69, 51, 101, 51, 79, 48,
+		     111, 48, 84, 55, 116, 55, 76, 49, 108, 49};
 	int i;
+	int j;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		switch (s[i] >= 0)
+		for (j = 0; j < 20; j++)
 		{
-		case 1:
-			if (s[i] == 65 || s[i] == 97)
-			s[i] = 52;
-		case 2:
-			if (s[i] == 69 || s[i] == 101)
-			s[i] = 51;
-		case 3:
-			if (s[i] == 79 || s[i] == 111)
-			s[i] = 48;
-		case 4:
-			if (s[i] == 84 || s[i] == 116)
-			s[i] = 55;
-		case 5:
-			if (s[i] == 76 || s[i] == 108)
-			s[i] = 49;
-
-
-		default:
-			break;
+			if (l[j] == s[i])
+			{
+				s[i] = l[++j];
+			}
 		}
 	}
 	return (s);
