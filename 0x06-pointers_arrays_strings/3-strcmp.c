8419ad118ae9e1b434c8c7969a4c51a8d2da1052
1270holberton <1270@holbertonschool.com>
2019-10-07 14:28:59 +0000

    task3-correction

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,38 +7,9 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-	int ret;
+	int i = 0;
 
-	for (i = 0; i != s1['\0']; i++)
-	{
-		ret = s1[i] - s2[i];
-		if (ret != 0)
-		{
-			break;
-		}
-	}
-<<<<<<< HEAD
-
-	while (s2[j] != '\0')
-	{
-		j++;
-	}
-
-	if (i > j)
-	{
-		x = i;
-	}
-	else if (i < j)
-	{
-		x = j;
-	}
-	else
-	{
-		x = 0;
-	}
-	return (x);
-=======
-	return (ret);
->>>>>>> 99bcf252d44b5e894b55241b1efe4b31f929dd35
+	while (s1[i] == s2[i] && s1[i] != '\0')
+		i++;
+	return (s1[i] - s2[i]);
 }
