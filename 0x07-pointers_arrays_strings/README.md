23ab7799f982575d522ffce4315277115e977045
1270holberton <1270@holbertonschool.com>
2019-10-07 15:59:27 +0000

    task0-mandatory

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is file README for 0x07-pointers_arrays_strings
---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,9 @@
+#ifndef _PUTCHAR_H
+#define _PUTCHAR_H
+int _putchar(char c);
+#endif
+
+#ifndef _MEMSET_H
+#define _MEMSET_H
+char *_memset(char *s, char b, unsigned int n);
+#endif
