eb2e33775337d850e6025ed459b14a8f3961c574
1270holberton <1270@holbertonschool.com>
2019-10-11 21:12:30 +0000

    task1-mandatory

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * main - prints its name, followed by a new line.
+ * main - prints the number of arguments passed into it.
  * @argc: count
  * @argv: vector
  * Return: output
  */
 int main(int argc, char **argv)
 {
-	(void)argc;
-	printf("%s\n", argv[0]);
+	(void)argv;
+	printf("%d\n", argc - 1);
 	return (0);
 }
