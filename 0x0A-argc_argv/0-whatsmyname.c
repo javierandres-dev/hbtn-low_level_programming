749246b5e61c906316530cd8b7d9d1a040c32dba
1270holberton <1270@holbertonschool.com>
2019-10-11 20:54:57 +0000

    task0-mandatory

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: count
+ * @argv: vector
+ */
+int main (int argc, char* argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
