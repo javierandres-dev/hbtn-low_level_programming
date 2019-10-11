e79a0d949709366df5f32730c6d7a22f8fc13394
1270holberton <1270@holbertonschool.com>
2019-10-11 22:18:30 +0000

    maintenance

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - program that multiplies two numbers.
+ * @argc: count
+ * @argv: vector
+ * Return: output
+ */
+int main(int argc, char **argv)
+{}
