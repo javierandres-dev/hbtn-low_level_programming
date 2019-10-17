7aa95064f1174b6c6a57d1e27bc165c3212719b3
1270holberton <1270@holbertonschool.com>
2019-10-17 22:09:57 +0000

    task2-correction

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,19 +9,19 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *ar;
+	char *ar;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
-	ar = malloc(sizeof(int) * nmemb);
+	ar = malloc(nmemb * size);
 	if (ar == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 	{
 		ar[i] = 0;
 	}
