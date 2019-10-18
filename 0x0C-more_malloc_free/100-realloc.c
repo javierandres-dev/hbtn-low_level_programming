b7958cbcb4e85bc96339ce638f57ed7ee9eccfc4
Javier Andrés <1270@holberton.com>
2019-10-18 22:34:51 +0000

    task4-correction

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,29 +12,35 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *ar;
 
-	ar = malloc(new_size);
-	if (ar == NULL)
-	{
-		return (NULL);
-	}
-	if (new_size > old_size)
-	{
-		ar[0] = new_size;
-	}
 	if (new_size == old_size)
 	{
 		return (ptr);
-		exit(1);
 	}
 	if (ptr == NULL)
 	{
-		return (ar);
+		ar = malloc(sizeof(char) * new_size);
+		if (ar == NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
+			free(ptr);
+			return (ar);
+		}
 	}
-	if (new_size == 0 && ptr != NULL)
+	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	free(ptr);
-	return (ar);
+	ar = malloc(sizeof(char) * new_size);
+	if (ar == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		return (ar);
+	}
 }
