928a75df842e45833a9d968d786454b7220a67cd
1270holberton <1270@holbertonschool.com>
2019-10-21 15:50:20 +0000

    task2man

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the name of the file it was
+ * compiled from, followed by a new line.
+ * Return: Always 0 (Success)
+ */
+int main ()
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
