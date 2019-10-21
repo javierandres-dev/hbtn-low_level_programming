6779a2253e2c0b53590bfe156cd6d51dafbcca45
1270holberton <1270@holbertonschool.com>
2019-10-21 19:05:36 +0000

    task0betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - Struct
+ * @name: member
+ * @age: member
+ * @owner: member
+ * Description: Define a new type
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-/* Define a new type */
 };
 #endif
