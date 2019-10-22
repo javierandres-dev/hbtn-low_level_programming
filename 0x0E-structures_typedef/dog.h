3804ba6df799a6a2022f3180385cf13810ef6ac4
1270holberton <1270@holbertonschool.com>
2019-10-22 14:15:41 +0000

    task4man

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,4 +21,6 @@ void print_dog(struct dog *d);
  */
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 #endif
