2309a31f73705f83cd3fb1139803a0ab636730bd
Javier Andrés <1270@holberton.com>
2019-10-22 03:42:58 +0000

    task3man

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,4 +15,9 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+/**
+ * typedef struct dog_t dog - Struct
+ * Description: Define a new type dog_t as a new name for the type struct dog
+ */
+typedef struct dog dog_t;
 #endif
