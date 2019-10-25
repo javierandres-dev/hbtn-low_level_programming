f18bac8ed75e12f785f877af4acaa2c37a5bbd1f
1270holberton <1270@holbertonschool.com>
2019-10-25 00:43:10 +0000

    task3man

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -12,6 +12,11 @@ void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 #endif
 
+#ifndef CONVERT_DAY_H
+#define CONVERT_DAY_H
+int convert_day(int month, int day);
+#endif
+
 #ifndef PRINT_REMAINING_DAYS_H
 #define PRINT_REMAINING_DAYS_H
 void print_remaining_days(int month, int day, int year);
