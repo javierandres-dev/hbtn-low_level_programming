6c9948bf9aea70a894a0e3af89b16f26595551c9
1270holberton <1270@holbertonschool.com>
2019-10-26 00:09:06 +0000

    task3init

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
+/**
+* print_all - function that prints anything.
+* @format: input
+* Return: output
+*/
+void print_all(const char * const format, ...)
+{}
