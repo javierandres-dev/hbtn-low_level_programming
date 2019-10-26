f70b18cee44cd7b063cd7a582a409ba8409b5ccd
1270holberton <1270@holbertonschool.com>
2019-10-26 21:03:54 +0000

    task3man

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,13 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+typedef struct op
+{
+	char *op;
+	void (*func)(va_list al);
+} op_t;
 #endif
