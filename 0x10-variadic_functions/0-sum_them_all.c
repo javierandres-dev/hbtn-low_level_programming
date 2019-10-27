928ba9f2064c6dc358c6216f47c750e58ee489dd
1270holberton <1270@holbertonschool.com>
2019-10-27 00:52:27 +0000

    task0fix5

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,15 +13,15 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0;
 
 	va_start(al, n);
+	if (n  == 0)
+	{
+		return (0);
+	}
 	i = 0;
 	while (i < n)
 	{
 		sum += va_arg(al, int);
 		i++;
-	}
-	if (n  == 0)
-	{
-		return (0);
 	}
 	va_end(al);
 	return (sum);
