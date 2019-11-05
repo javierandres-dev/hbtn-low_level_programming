ceba45093d0a1f6e775769f13b56e0bf4607babb
1270holberton <1270@holbertonschool.com>
2019-11-05 22:14:58 +0000

    update

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - function that
+ * inserts a new node at a given position.
+ * @head: input
+ * idx: input
+ * @n: input
+ * Return: output
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+{}
