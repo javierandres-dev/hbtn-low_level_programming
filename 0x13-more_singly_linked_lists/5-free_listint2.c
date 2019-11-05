d66d631f493bcfdf4f765bef474693cd70d3d333
1270holberton <1270@holbertonschool.com>
2019-11-05 18:36:57 +0000

    task5fix4

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,10 +8,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	while (*head != NULL)
+	if (head != NULL)
 	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
+		while (*head != NULL)
+		{
+			tmp = (*head)->next;
+			free(*head);
+			*head = tmp;
+		}
+		*head = NULL;
 	}
 }
