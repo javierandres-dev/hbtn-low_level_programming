25fa84788f038dcd4a178c4d57b99f841bf85821
1270holberton <1270@holbertonschool.com>
2019-11-05 18:16:32 +0000

    task5man

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - function that frees a listint_t list.
+ * @head: input
+ * Return: output
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (*head != NULL)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+	*head = NULL;
+}
