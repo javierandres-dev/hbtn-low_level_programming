43d26570b2d582ae0642f27a22490a06d4480190
1270holberton <1270@holbertonschool.com>
2019-11-06 18:34:46 +0000

    task9fix3

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,6 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 	{
+		free(new);
 		return (NULL);
 	}
 	new->n = n;
