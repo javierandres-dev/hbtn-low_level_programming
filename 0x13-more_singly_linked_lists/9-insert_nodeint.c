70caac89d55cf95f7031f0dc215221708ce3e27a
1270holberton <1270@holbertonschool.com>
2019-11-06 17:25:02 +0000

    task9man

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,9 +3,30 @@
  * insert_nodeint_at_index - function that
  * inserts a new node at a given position.
  * @head: input
- * idx: input
+ * @idx: input
  * @n: input
  * Return: output
  */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
-{}
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new;
+	listint_t *tmp = *head;
+	unsigned int i = 0;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		new->n = n;
+		while (head != NULL && (idx - 1) == i)
+		{
+			new->next = tmp->next;
+			tmp->next = new;
+			i++;
+		}
+		return (new);
+	}
+}
