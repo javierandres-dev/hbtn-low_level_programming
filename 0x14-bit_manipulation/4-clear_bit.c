0107158f798c5b6296dea08eb1adfbc63ac1fe07
1270holberton <1270@holbertonschool.com>
2019-11-08 17:27:37 +0000

    task4man

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,4 +7,17 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	long int set = 1;
+
+	if (index < 64)
+	{
+		set = ~(1 << index);
+		*n = *n & set;
+		return (1);
+	}
+	else
+	{
+		return (-1);
+	}
+
 }
