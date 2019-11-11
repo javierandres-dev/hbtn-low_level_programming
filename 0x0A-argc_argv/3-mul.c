39bed548fbb8f5dd5f88eda41c337e4bda2ef4fa
1270holberton <1270@holbertonschool.com>
2019-11-11 18:42:36 +0000

    task3man

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * main - program that multiplies two numbers.
  * @argc: count
@@ -7,4 +9,23 @@
  * Return: output
  */
 int main(int argc, char **argv)
-{}
+{
+	int n1;
+	int n2;
+	int result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		result = n1 * n2;
+
+		printf("%d\n", result);
+	}
+	return (0);
+}
