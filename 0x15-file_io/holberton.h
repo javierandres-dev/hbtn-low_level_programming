b7c65d6ffe91d0403c84a5f4ebc29455ee1f6271
1270holberton <1270@holbertonschool.com>
2019-11-12 19:31:25 +0000

    task0man

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif
