e068468cf8973165811ccf4a38ad3d7eb324de9c
1270holberton <1270@holbertonschool.com>
2019-11-13 00:26:11 +0000

    task1manfix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,18 +16,20 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 	{
 		return (-1);
 	}
-	for (i = 0; text_content[i] != 0; i++)
+	i = 0;
+	while (text_content[i] != '\0')
 	{
-		writing = write(fd, text_content, i);
-		if (writing == -1)
-		{
-			return (-1);
-		}
+		i++;
+	}
+	writing = write(fd, text_content, i);
+	if (writing == -1)
+	{
+		return (-1);
 	}
 	close(fd);
 	return (1);
