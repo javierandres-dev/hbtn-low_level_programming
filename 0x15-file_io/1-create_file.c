4ee78effd85c54b9925e5be3a12ff536c6f6b2a5
1270holberton <1270@holbertonschool.com>
2019-11-13 00:46:04 +0000

    task2man

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,16 +23,16 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content)
 	{
-	i = 0;
-	while (text_content[i] != '\0')
-	{
-		i++;
-	}
-	writing = write(fd, text_content, i);
-	if (writing == -1)
-	{
-		return (-1);
-	}
+		i = 0;
+		while (text_content[i] != '\0')
+		{
+			i++;
+		}
+		writing = write(fd, text_content, i);
+		if (writing == -1)
+		{
+			return (-1);
+		}
 	}
 	close(fd);
 	return (1);
