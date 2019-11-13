f3ae7cbbd8294d6184131707b86eb2f8195ce19d
1270holberton <1270@holbertonschool.com>
2019-11-13 00:05:52 +0000

    task1man

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,16 +3,32 @@
  * create_file - function that creates a file.
  * @filename: is the name of the file to create
  * @text_content: is a NULL terminated string to write to the file
- * Return: 1 on success, -1 on failure
- * (file can not be created, file can not be written, write “fails”, etc…)
+ * Return: 1 on success, -1 on failure (file can not be created,
+ * file can not be written, write “fails”, etc…)
  */
 int create_file(const char *filename, char *text_content)
 {
-	char *buf;
+	int i;
 	int fd;
+	int writing;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	for (i = 0; text_content[i] != 0; i++)
+	{
+		writing = write(fd, text_content, i);
+		if (writing == -1)
+		{
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
 }
