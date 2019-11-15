f8c7872112fcc6dca1a6dc95ea779f0b1ddb669f
1270holberton <1270@holbertonschool.com>
2019-11-15 22:41:32 +0000

    update-all-names

--- 0x16-simple_shell/shell.c
+++ 0x16-simple_shell/shell.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -10,4 +11,114 @@
  */
 int main()
 {
+	pid_t child1_pid, child2_pid, child3_pid, child4_pid, child5_pid;
+	int status1, status2, status3, status4, status5;
+	char *argv[] = {"prompt", NULL};
+
+	child1_pid = fork();
+	if (child1_pid == -1)
+	{
+		perror("Error:");
+		return (1);
+	}
+	if (child1_pid == 0)
+	{
+		printf("I'm the first child (%d, son of %d)\n", getpid(), getppid());
+		if (execve(argv[0], argv, NULL) == -1)
+		{
+			perror("Error:");
+		}
+		printf("Command executed by first child\n");
+	}
+	else
+	{
+		wait(&status1);
+		printf("  OK for first child\n");
+		printf("  I'm the father (%d, son of %d)\n", getpid(), getppid());
+		child2_pid = fork();
+		if (child2_pid == -1)
+		{
+			perror("Error:");
+			return (1);
+		}
+		if (child2_pid == 0)
+		{
+			printf("I'm the second child (%d, son of %d)\n", getpid(), getppid());
+			if (execve(argv[0], argv, NULL) == -1)
+			{
+				perror("Error:");
+			}
+			printf("Command executed by second child\n");
+		}
+		else
+		{
+			wait(&status2);
+			printf("  OK for second child\n");
+			printf("  I'm the father (%d, son of %d)\n", getpid(), getppid());
+			child3_pid = fork();
+			if (child3_pid == -1)
+			{
+				perror("Error:");
+				return (1);
+			}
+			if (child3_pid == 0)
+			{
+				printf("I'm the third child (%d, son of %d)\n", getpid(), getppid());
+				if (execve(argv[0], argv, NULL) == -1)
+				{
+					perror("Error:");
+				}
+				printf("Command executed by third child\n");
+			}
+			else
+			{
+				wait(&status3);
+				printf("  OK for third child\n");
+				printf("  I'm the father (%d, son of %d)\n", getpid(), getppid());
+				child4_pid = fork();
+				if (child4_pid == -1)
+				{
+					perror("Error:");
+					return (1);
+				}
+				if (child4_pid == 0)
+				{
+					printf("I'm the fourth child (%d, son of %d)\n", getpid(), getppid());
+					if (execve(argv[0], argv, NULL) == -1)
+					{
+						perror("Error:");
+					}
+					printf("Command executed by fourth child\n");
+				}
+				else
+				{
+					wait(&status4);
+					printf("  OK for fourth child\n");
+					printf("  I'm the father (%d, son of %d)\n", getpid(), getppid());
+					child5_pid = fork();
+					if (child5_pid == -1)
+					{
+						perror("Error:");
+						return (1);
+					}
+					if (child5_pid == 0)
+					{
+						printf("I'm the fifth child (%d, son of %d)\n", getpid(), getppid());
+						if (execve(argv[0], argv, NULL) == -1)
+						{
+							perror("Error:");
+						}
+						printf("Command executed by fifth child\n");
+					}
+					else
+					{
+						wait(&status5);
+						printf("  OK for fifth child\n");
+						printf("  I'm the father (%d, son of %d)\n", getpid(), getppid());
+					}
+				}
+			}
+		}
+	}
+        return (0);
 }
