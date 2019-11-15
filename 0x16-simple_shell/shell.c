8241650d3dd9b3935277ad53aead65c4117f584a
1270holberton <1270@holbertonschool.com>
2019-11-15 20:43:28 +0000

    name-change

---
+++ 0x16-simple_shell/shell.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+/**
+ * main - first version of a super simple shell that can
+ * run commands with their full path, without any argument.
+ *
+ * Return: Always 0.
+ */
+int main()
+{
+}
