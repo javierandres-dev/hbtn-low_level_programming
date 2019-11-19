8511820154acc488fd11e44f5ebcfb08b3cff475
1270holberton <1270@holbertonschool.com>
2019-11-19 23:17:15 +0000

    update

--- 0x16-simple_shell/super-simple-shell/five-childs.c
+++ 0x16-simple_shell/super-simple-shell/five-childs.c
@@ -10,114 +10,34 @@
  */
 int main(void)
 {
-	pid_t child1_pid, child2_pid, child3_pid, child4_pid, child5_pid;
-	int status1, status2, status3, status4, status5;
+	pid_t child_pid;
+	int status;
 	char *argv[] = {"/bin/ls", "-l", "/tmp/", NULL};
+	int i = 0;
 
-	child1_pid = fork();
-	if (child1_pid == -1)
+	while (i < 5)
 	{
-		perror("Error:");
-		return (1);
-	}
-	if (child1_pid == 0)
-	{
-		printf("I'm the first child (%d, son of %d)\n", getpid(), getppid());
-		if (execve(argv[0], argv, NULL) == -1)
-		{
-			perror("Error:");
-		}
-		printf("Command executed by first child\n");
-	}
-	else
-	{
-		wait(&status1);
-		printf("  OK for first child\n");
-		printf("  I'm the father (%d, son of %d)\n", getpid(), getppid());
-		child2_pid = fork();
-		if (child2_pid == -1)
+		child_pid = fork();
+		if (child_pid == -1)
 		{
 			perror("Error:");
 			return (1);
 		}
-		if (child2_pid == 0)
+		if (child_pid == 0)
 		{
-			printf("I'm the second child (%d, son of %d)\n", getpid(), getppid());
+			printf("I'm child, my PID is: %d, I'm son of: %d\n", getpid(), getppid());
 			if (execve(argv[0], argv, NULL) == -1)
 			{
 				perror("Error:");
 			}
-			printf("Command executed by second child\n");
+			printf("Command executed by child: %d\n", getpid());
 		}
 		else
 		{
-			wait(&status2);
-			printf("  OK for second child\n");
-			printf("  I'm the father (%d, son of %d)\n", getpid(), getppid());
-			child3_pid = fork();
-			if (child3_pid == -1)
-			{
-				perror("Error:");
-				return (1);
-			}
-			if (child3_pid == 0)
-			{
-				printf("I'm the third child (%d, son of %d)\n", getpid(), getppid());
-				if (execve(argv[0], argv, NULL) == -1)
-				{
-					perror("Error:");
-				}
-				printf("Command executed by third child\n");
-			}
-			else
-			{
-				wait(&status3);
-				printf("  OK for third child\n");
-				printf("  I'm the father (%d, son of %d)\n", getpid(), getppid());
-				child4_pid = fork();
-				if (child4_pid == -1)
-				{
-					perror("Error:");
-					return (1);
-				}
-				if (child4_pid == 0)
-				{
-					printf("I'm the fourth child (%d, son of %d)\n", getpid(), getppid());
-					if (execve(argv[0], argv, NULL) == -1)
-					{
-						perror("Error:");
-					}
-					printf("Command executed by fourth child\n");
-				}
-				else
-				{
-					wait(&status4);
-					printf("  OK for fourth child\n");
-					printf("  I'm the father (%d, son of %d)\n", getpid(), getppid());
-					child5_pid = fork();
-					if (child5_pid == -1)
-					{
-						perror("Error:");
-						return (1);
-					}
-					if (child5_pid == 0)
-					{
-						printf("I'm the fifth child (%d, son of %d)\n", getpid(), getppid());
-						if (execve(argv[0], argv, NULL) == -1)
-						{
-							perror("Error:");
-						}
-						printf("Command executed by fifth child\n");
-					}
-					else
-					{
-						wait(&status5);
-						printf("  OK for fifth child\n");
-						printf("  I'm the father (%d, son of %d)\n", getpid(), getppid());
-					}
-				}
-			}
+			wait(&status);
+			printf("I'm father, my PID is: %d\n", getpid());
 		}
+		i++;
 	}
 	return (0);
 }
--- 0x16-simple_shell/super-simple-shell/shell.c
+++ 0x16-simple_shell/super-simple-shell/shell.c
@@ -3,6 +3,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <string.h>
 /**
  * main - first version of a super simple shell that can
  * run commands with their full path, without any argument.
@@ -13,48 +14,75 @@ int main()
 {
 	pid_t child;
 	int status;
-	int i = 0;
+	char *line;
+	size_t len;
+	const char split[2] = " \n";
+	char *run;
+	char **exe;
+	int i;
 
-	while (i < 5)
+	while (1)
 	{
 		child = fork();
 		if (child < 0)
 		{
 			perror("Error: CHILD < 0 !!!\n");
-			return (1);
+			return (-1);
 		}
 		if (child == 0)
 		{
-			sleep(2);
-			printf("I'm child, my PID is: %d and my father is: %d\n", getpid(), getppid());
-			_exit(0);
+			line = malloc(sizeof(char) * len);
+			if (line == NULL)
+			{
+				free(line);
+				return (0);
+			}
+			printf("#cisfun$ ");
+			getline(&line, &len, stdin);
+			//printf("LINE: %s", line);
+			run = strtok(line, split);
+			exe = malloc(sizeof(char) * len);
+			//printf("LEN: %i\n", (int) len);
+			if (line == NULL)
+			{
+				free(line);
+				return (0);
+			}
+			i = 0;
+			while(run != NULL)
+			{
+				//printf("RUN: %s\n", run);
+				exe[i] = run;
+				//printf("EXE: %s\n", exe[i]);
+				run = strtok(NULL, split);
+				i++;
+			}
+			exe[i] = 0;
+			if (execve(exe[0], exe, NULL) == -1)
+			{
+				perror("Error: !!!");
+			}
 		}
 		if (child > 0)
 		{
 			wait(&status);
-			printf("I'm father, my PID is: %d\n", getpid());
 		}
-		i++;
+		//free(line);
+		//free(exe);
 	}
 	return (0);
 }
 /*
-  char *line;
-  size_t len;
-  line = malloc(sizeof(char) * len);
-  if (line == NULL)
-  {
-  free(line);
-  return (0);
-  }
-  printf("#cisfun$ ");
-  getline(&line, &len, stdin);
+
+	int i = 0;
+	int j = 0;
+	char **run;
+
+
   printf("You wrote: %s", line);
-  char *argv[2];
-  argv[0] = "prompt";
-  argv[1] = NULL;
-  if (execve(argv[0], argv, NULL) == -1)
-  {
-  perror("Error: !!!");
-  }
+  i++;
+  sleep(1);
+  _exit(0);
+  printf("I'm child, my PID is: %d and my father is: %d\n", getpid(), getppid());
+  printf("I'm father, my PID is: %d\n", getpid());
 */
