f182d4ee16ffca422615eaa644f46c2d259be496
1270holberton <1270@holbertonschool.com>
2019-11-23 02:35:07 +0000

    exercise _which

--- 0x16-simple_shell/exercises/_which.c
+++ 0x16-simple_shell/exercises/_which.c
@@ -1,27 +1,34 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <string.h>
 /**
  * main - program that looks for files in the current PATH.
  *
  * Return: Always 0.
  */
-int main (int c, char *v[]) {
-  struct dirent *pDirent;
-  DIR *pDir;
+int main ()
+{
+	struct dirent *filename;
+	DIR *dir;
+	char s[16];
+	int result;
 
-  if (c < 2) {
-    printf ("Usage: testprog <dirname>\n");
-    return 1;
-  }
-  pDir = opendir (v[1]);
-  if (pDir == NULL) {
-    printf ("Cannot open directory '%s'\n", v[1]);
-    return 1;
-  }
-
-  while ((pDirent = readdir(pDir)) != NULL) {
-    printf ("[%s]\n", pDirent->d_name);
-  }
-  closedir (pDir);
-  return 0;
+	scanf("%s", s);
+	if((dir = opendir(".")) == NULL)
+	{
+		perror("opendir() error");
+	}
+	else
+	{
+		while((filename = readdir(dir)) != NULL)
+		{
+			result = strcmp(s, filename->d_name);
+			if (result == 0) 
+			{
+				printf("Success! The current PATH if it contains: %s\n", filename->d_name);
+			}
+		}
+	}
+	closedir(dir);
+	return (0);
 }
