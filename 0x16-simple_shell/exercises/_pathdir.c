541becc1cb0831d329e1dd51e020a5eea4a60af1
1270holberton <1270@holbertonschool.com>
2019-11-26 04:05:14 +0000

    update

--- 0x16-simple_shell/exercises/_pathdir.c
+++ 0x16-simple_shell/exercises/_pathdir.c
@@ -1,36 +1,25 @@
 #include "exercises.h"
 /**
-* _getenv - function that gets an environment variable. (without using getenv)
+* main - function that prints each directory contained in the the environment variable PATH, one directory per line.
 */
-extern char **environ;
 int main()
-//char *_getenv(const char *name)
 {
+	extern char **environ;
 	char *name = "PATH";
-	char *check1;
-	char *check2;
-	int result1;
-	int result2;
+	char *check;
+	int result;
 	int i;
-	//char *name = "USER=vagrant";
 	i = 0;
 	while(environ[i] != NULL)
 	{
-		//check = strtok(environ[i], "=");
-		check1 = strtok(environ[i], "=");
-		//check2 = strtok(environ[i], ":");
-		//result = strcmp(name, environ[i]);
-		result1 = strcmp(name, check1);
-		//result2 = strcmp(name, check2);
-		if (result1  == 0)
+		check = strtok(environ[i], "=");
+		result = strcmp(name, check);
+		if (result  == 0)
 		{
-			//printf("%s\n", environ[i]);
-			check1 = strtok(NULL, "\0");
-			//return (check);
-			printf("Print PATH: %s\n", check1);
+			check = strtok(NULL, "\0");
+			printf("Print PATH: %s\n", check);
 		}
 		i++;
 	}
-	//return (NULL);
 	return (0);
 }
