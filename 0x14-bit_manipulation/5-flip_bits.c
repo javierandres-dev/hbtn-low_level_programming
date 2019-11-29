41751256625fdfe3ab8d492f2cc56a8745eecd01
1270holberton <1270@holbertonschool.com>
2019-11-29 14:11:58 -0500

    update

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -8,4 +8,15 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned long int r;
+	int count = 0;
+
+	r = n ^ m;
+
+	while (r != 0)
+	{
+		count += r & 1;
+		r = r >> 1;
+	}
+	return (count);
 }
