4a4673db4b9418634a6fbcdb476b0120d8c5b71a
1270holberton <1270@holbertonschool.com>
2019-12-14 22:25:06 +0000

    task1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * dlistint_len - function that returns the number of elements in a linked dlistint_t list.
+ * @h: input
+ * Return: number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *element = h;
+	size_t i = 0;
+
+	while (element != NULL)
+	{
+		element = element->next;
+		i++;
+	}
+	return (i);
+}
