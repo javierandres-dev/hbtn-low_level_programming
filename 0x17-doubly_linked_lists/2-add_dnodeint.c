c7034b9c77d024fc26903ca9e05ffe308ffdf44b
1270holberton <1270@holbertonschool.com>
2019-12-14 23:24:55 +0000

    task2fix

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -21,6 +21,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		new->prev = NULL;
 		new->next = *head;
 	}
+	if (*head != NULL)
+	{
+		(*head)->prev = new;
+	}
 	*head = new;
 	return (new);
 }
