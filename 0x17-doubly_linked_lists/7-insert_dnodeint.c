6921e669c54c2e00db757b09a5acc800aba1ced8
1270holberton <1270@holbertonschool.com>
2019-12-15 20:40:10 +0000

    task7fix1

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,6 +12,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new, *tmp = *h;
 	unsigned int i = 0;
 
+	if (!h)
+		return (NULL);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 	if (idx > 0 && *h == NULL)
