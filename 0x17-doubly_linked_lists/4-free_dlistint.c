9a9e15e425551c104063ac8ecda706cff520859a
1270holberton <1270@holbertonschool.com>
2019-12-15 00:08:09 +0000

    task4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_dlistint - function that free a dlistint_t list.
+ * @head: input
+ * Return: output
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp = head;
+
+	while (head != NULL)
+	{
+		head = head->next;
+		free(tmp);
+		tmp = head;
+	}
+	free(head);
+}
