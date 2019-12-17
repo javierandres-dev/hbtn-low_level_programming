a02f08bee2a13ad869c3f697cccb21b2282fcce9
1270holberton <1270@holbertonschool.com>
2019-12-17 00:33:03 +0000

    task0

---
+++ 0x18-dynamic_libraries/_puts.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @str: input
+ */
+void _puts(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
+}
