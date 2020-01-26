c358f7bca922f85ae5ff9ea952c2fe47d9477f12
1270holberton <1270@holbertonschool.com>
2020-01-26 14:38:25 -0500

    task3fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,10 +1,11 @@
 #include "hash_tables.h"
 /**
- * hash_tablet_set - function that adds an element to the hash table
+ * hash_table_set - function that adds an element to the hash table
+ * @ht: is the hash table
  * @key: is the key. key can not be an empty string
  * @value: is the value associated with the key.
  * value must be duplicated. value can be an empty string
- * Returns: 1 if it succeeded, 0 otherwise
+ * Return: 1 if it succeeded, 0 otherwise
  * In case of collision, add the new node at the beginning of the list
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
@@ -14,23 +15,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index = 0;
 
 	if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
-	{
 		return (0);
-	}
 	index  = key_index((unsigned char *)key, ht->size);
 	aux = ht->array[index];
 	while (aux && strcmp(aux->key, key) != 0)
-	{
 		aux = aux->next;
-	}
 	if (aux)
 	{
 		free(aux->value);
 		aux->value = strdup(value);
 		if (aux->value == NULL)
-		{
 			return (0);
-		}
 		return (1);
 	}
 	new = malloc(sizeof(hash_node_t));
