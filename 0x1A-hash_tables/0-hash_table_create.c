c72a458e9710c73c554f90d30ca01c86b786aa7b
1270holberton <1270@holbertonschool.com>
2020-01-26 13:35:21 -0500

    task0fix1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,12 +17,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 		new = malloc(sizeof(hash_table_t));
 		if (new == NULL)
 		{
+			free(new);
 			return (NULL);
 		}
 		new->size = size;
-		new->array = malloc(sizeof(hash_node_t *) * size);
+		new->array = calloc(size, sizeof(hash_node_t *));
 		if (new->array == NULL)
 		{
+			free(new);
 			return (NULL);
 		}
 		return (new);
